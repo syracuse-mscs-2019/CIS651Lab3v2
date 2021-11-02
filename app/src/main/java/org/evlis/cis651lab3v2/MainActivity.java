7e266ef47e1ab684638f02da5d5a42fe742a1281
Peter Pan <peter@never.lan>
2021-11-02 07:40:49 -0400

    Initial commit

---
+++ app/src/main/java/org/evlis/cis651lab3v2/MainActivity.java
@@ -0,0 +1,13 @@
+package org.evlis.cis651lab3v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
